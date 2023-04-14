a8e1c79a5b7afa90f50c43c2680ab9fa70a90899
Saad Selim <saadselim6@gmail.com>
2023-04-14 07:28:28 -0400

    003

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={"text-blue-400 text-7xl "}>Welcome to</h1>
+    <main>
+      <h1>Hello there everyone</h1>
     </main>
   );
 }
